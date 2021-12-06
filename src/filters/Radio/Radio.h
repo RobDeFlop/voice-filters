4ed205bdec9de0e806733a0ce23b77a683244988
RobDeFlop <domeme.ks@gmail.com>
2021-12-06 02:00:28 +0100

    fixed dspfilters std::min,max macro and fixed CMakeList

--- src/filters/Radio/Radio.h
+++ src/filters/Radio/Radio.h
@@ -2,25 +2,25 @@
 
 #include "main.h"
 #include "RadioSettings.h"
-#include "Dsp/Bessel.h"
+#include "DspFilters/Dsp.h"
+#include <memory>
 
-class Radio : public alt::IAudioFilter {
+class Radio : public alt::IAudioFilter
+{
 public:
     Radio(){};
     ~Radio(){};
 
-    void SetParam(const size_t& key, bool value) override;
-    void GetParam(const size_t& key, bool& value) override;
-    void Process(pcmSample* data, uint32_t size) override;
+    void SetParam(const size_t &key, bool value) override;
+    void GetParam(const size_t &key, bool &value) override;
+    void Process(pcmSample *data, uint32_t size) override;
 
 private:
     RadioSettings m_settings;
 
-    //std::unique_ptr<Dsp::Filter> f = std::make_unique<Dsp::SmoothedFilterDesignDsp::Butterworth::Design::BandPass<4>,7, Dsp::DirectFormII>>(1024);
-    //std::unique_ptr<Dsp::Filter> f_o = std::make_unique<Dsp::SmoothedFilterDesignDsp::Butterworth::Design::BandPass<4>,7, Dsp::DirectFormII>>(1024);
+    std::unique_ptr<Dsp::Filter> f = std::make_unique<Dsp::SmoothedFilterDesign<Dsp::Butterworth::Design::BandPass<4>, 6, Dsp::DirectFormII>>(1024);
+    std::unique_ptr<Dsp::Filter> f_o = std::make_unique<Dsp::SmoothedFilterDesign<Dsp::Butterworth::Design::BandPass<4>, 6, Dsp::DirectFormII>>(1024);
 
     std::pair<double, double> m_last_eq_in;
     std::pair<double, double> m_last_eq_out;
 };
-
-
